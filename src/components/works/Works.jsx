464188bb635572d2e1abc91a33ef070ff988f089
atthoriqgf <atthoriqgf@gmail.com>
2021-10-12 12:31:25 +0700

    init commit

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,16 @@
 import "./works.scss";
 
 export default function Work() {
-  return <div className="works" id="works"></div>;
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
